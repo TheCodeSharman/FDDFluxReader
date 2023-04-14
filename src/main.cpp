5653b7c0546f2e6656fac2665e2548df703d028b
Michael Sharman <michael@thecodesharman.com.au>
2023-04-14 22:49:35 +1000

    Set up a PWM signal source
    This lets us set the preice frequency to attempt to measure
    at about 6us the interrupt based sampler seems to miss transitions.
    
    Looks like the ISR is too slow to keep to with the input changes.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,9 @@
 #include "CommandProcessor.h"
 #include "PinSampler.h"
 
-const uint8_t LED_PIN = PC13;
-const uint8_t READ_PIN = PA1;
+const uint32_t LED_PIN = PC13;
+const uint32_t READ_PIN = PA1;
+const uint32_t TEST_SIGNAL_PIN = PA10;
 
 MultiTask tasks;
 PinSampler readSampler(Serial,tasks,READ_PIN);
@@ -17,6 +18,8 @@ void blinkLed() {
   isLedOn = !isLedOn;
 }
 
+HardwareTimer testSignal;
+
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   pinMode(READ_PIN, INPUT);   
@@ -24,8 +27,11 @@ void setup() {
   tasks.every(500000,blinkLed);
   command.init();
 
-  pinMode(PA_10,OUTPUT);
-  analogWrite(PA_10,127);
+  TIM_TypeDef *instance = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(TEST_SIGNAL_PIN), PinMap_TIM);
+  uint32_t channel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(TEST_SIGNAL_PIN), PinMap_TIM));
+  testSignal.setup(instance);
+  testSignal.setPWM(channel, TEST_SIGNAL_PIN, 333333/2, 50);
+
   readSampler.init();
 }
 
