a987a05d067a5e8849508f69253150183ae708b5
Michael Sharman <michael@thecodesharman.com.au>
2023-04-30 14:55:05 +1000

    Adds some documenaton on BufferEncoder

--- lib/BufferEncoder/include/BufferEncoder.h
+++ lib/BufferEncoder/include/BufferEncoder.h
@@ -4,12 +4,28 @@
 #include <cstddef>
 #include <cstdint>
 
-class BufferEncoder {
+/**
+ * Encodes an array of samples into a compressed ASCII string in order to reduce the bandwidth
+ * needed to send down the serial line. Most samples are between 2uS and 7uS, this algorihtm
+ * aims to exploit this to, on average, send one byte per sample, but allowing longer pulses to
+ * occassionally be present.
+ * 
+ * This implementation does the following:
+ *          - for each sample:
+ *           -  subtracts 100 from the sample 
+ *           -  if the sample < 0x7f then store it in a single byte
+ *           -  if the sample >= 0x7f then store rest of the word in multiple bytes
+ *           -  the MSB bit (0x80) of a byte is set when more bytes to follow
+ *          - base64 encode the resulting 
+ * 
+ * Decoding is the reverse of the above.
+ */
 
+class BufferEncoder {
     public:
-    size_t maxBufferLength( const size_t count);
-    int decodeSampleBuffer( const size_t inputBufferSize, const uint8_t *base64Buffer, uint32_t *samples );
-    int encodeSampleBuffer( const size_t count, uint8_t *base64Buffer, const uint32_t *samples);
+        size_t maxBufferLength( const size_t count);
+        int decodeSampleBuffer( const size_t inputBufferSize, const uint8_t *base64Buffer, uint32_t *samples );
+        int encodeSampleBuffer( const size_t count, uint8_t *base64Buffer, const uint32_t *samples);
 
 };
 
